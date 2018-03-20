938095ebec7a6f77d8e9373b9d8f8c2cecc7969a
Malcolm Strong <strong.malcolm@yahoo.com>
2018-03-19 21:08:46 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+1. Write a function that returns either a letter grade “A” “B” … or "out of range" if it is not a proper grade when a numeric grade is passed in to that function: 90 - 100 A80 - 89 B72 - 79 C0 - 71 F
+
+2. Write JS/JQuery code that sets focus to an anchor tag with id="main" when a button with class="skip" is clicked. <button class=”skip”>Skip to Main</button><a id=”main”>...</a>
+
+3. Write JS/JQuery code to give the class “learn” to all links on a page that have the text “learn more”: <a href=”....”>learn more</a>
---
+++ assets/javascript.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+  $("#get-grade").on("click",function(event) {
+    event.preventDefault();
+      // $("#main").focus("button").css("background-color", "red");
+
+  var grade = $("#grade-input").val();
+
+  function letterGrade(grade) {
+
+    if ( grade > 100 || grade < 0 ) {
+      return 'INVALID grade';
+    }
+
+    if ( grade >= 90 ) {
+      if ( grade <= 92 ) {
+        return 'A-';
+      } else if ( grade >= 98 ) {
+        return 'A+';
+      } else {
+        return 'A';
+      }
+    } else if ( grade >= 80 ) {
+        if ( grade <= 82 ) {
+          return 'B-';
+        } else if ( grade >= 88 ) {
+          return 'B+';
+        } else {
+          return 'B';
+        }
+    } else if ( grade >= 70 ) {
+        if ( grade <= 72 ) {
+          return 'C-';
+        } else if ( grade >= 78 ) {
+          return 'C+';
+        } else {
+          return 'C';
+        }
+    } else if ( grade >= 60 ) {
+        if ( grade <= 62 ) {
+          return 'D-';
+        } else if ( grade >= 68 ) {
+          return 'D+';
+        } else {
+          return 'D';
+        }
+    } else {
+        return 'out of range';
+    }
+
+  }
+
+  var output = letterGrade(grade);
+  alert("Grade: " + output);
+
+});
+
+$(".skip").click(function() {
+  $("#main").focus();
+});
+
+
+
+
+
+});
